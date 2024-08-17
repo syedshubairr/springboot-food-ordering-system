7861c33e8244103100fc9c5f5fc4e4281461de93
Syed Shubair <shubair@chargeup.club>
2024-08-18 00:41:06 +0500

    Added Auth Controller with signup method.

--- src/main/java/com/shah/foodwesbite/response/AuthResponse.java
+++ src/main/java/com/shah/foodwesbite/response/AuthResponse.java
@@ -1,7 +1,9 @@
 package com.shah.foodwesbite.response;
 
 import com.shah.foodwesbite.entity.USER_ROLE;
+import lombok.Data;
 
+@Data
 public class AuthResponse {
     private String jwt;
     private String message;
